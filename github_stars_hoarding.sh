39b7f16b9b25d96338313aa24fafe0b4c51f7833
Abzwingt <abzwingt@gmail.com>
2024-01-16 19:40:10 +0300

    Create github_stars_hoarding.sh

---
+++ github_stars_hoarding.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+user="$1"
+user="$github_username"
+pages=$(curl -I https://api.github.com/users/$user/starred | sed -nr 's/^link:.*page=([0-9]+).*/\1/p')
+
+for page in $(seq 1 $pages); do
+    curl "https://api.github.com/users/$user/starred?page=$page&per_page=30" |   jq -r '.[].html_url'  | 
+    while read rp; do
+      if [ ! -d "$rp" ]; then
+              git clone $rp
+        else
+                git pull $rp
+        fi
+    done
+done
