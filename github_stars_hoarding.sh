6525a79aa573646b87554d61ed4777f2bc5b21bb
Abzwingt <52573322+Abzwingten@users.noreply.github.com>
2024-06-10 15:04:06 +0300

    Update github_stars_hoarding.sh

--- github_stars_hoarding.sh
+++ github_stars_hoarding.sh
@@ -17,22 +17,22 @@ echo -e "${red_bg}${reset}"
 echo -e "${blue}Welcome to github stars downloader!${reset}"
 pages=$(curl -I https://api.github.com/users/$user/starred | sed -nr 's/^Link:.*page=([0-9]+).*/\1/p')
 repolist_appr=$(expr $per_page \* $pages)
-echo "${blue}${uline} $pages ${reset} pages of repos about to be synced"
-echo "starting download"
+echo -e "${blue}${uline} $pages ${reset} pages of repos about to be synced"
+echo -e "starting download"
 cloned=0
 pulled=0
 for page in $(seq 1 $pages); do
     curl "https://api.github.com/users/$user/starred?page=$page&per_page=$per_page" |   jq -r '.[].html_url' | \
         while read rp; do
                 rp_name=${rp##*/}
-                echo  "URL: $rp"
+               # echo "URL: $rp"
                 if [ ! -d "$rp_name" ]; then
-                        git clone $rp
+                        git clone $rp >/dev/null
                         cloned=$((cloned+1))
                         echo "$rp_name cloned"
                 else
                         cd $rp_name
-                        git pull
+                        git pull >/dev/null
                         pulled=$((pulled+1))
                         echo "$rp_name updated"
                 fi
