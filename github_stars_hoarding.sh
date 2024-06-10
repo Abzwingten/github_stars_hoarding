b25a5685034643bde676f1effda4e72df7530ff7
Abzwingt <52573322+Abzwingten@users.noreply.github.com>
2024-06-10 15:06:41 +0300

    Update github_stars_hoarding.sh

--- github_stars_hoarding.sh
+++ github_stars_hoarding.sh
@@ -15,24 +15,24 @@ cd "$(dirname "$0")"
 user="${1:-Abzwingten}"
 echo -e "${red_bg}${reset}"
 echo -e "${blue}Welcome to github stars downloader!${reset}"
-pages=$(curl -I https://api.github.com/users/$user/starred | sed -nr 's/^Link:.*page=([0-9]+).*/\1/p')
+pages=$(curl -s -I https://api.github.com/users/$user/starred | sed -nr 's/^Link:.*page=([0-9]+).*/\1/p')
 repolist_appr=$(expr $per_page \* $pages)
-echo -e "${blue}${uline} $pages ${reset} pages of repos about to be synced"
+echo -e "${blue}${bold}${uline} $pages ${reset} pages of repos about to be synced"
 echo -e "starting download"
 cloned=0
 pulled=0
 for page in $(seq 1 $pages); do
-    curl "https://api.github.com/users/$user/starred?page=$page&per_page=$per_page" |   jq -r '.[].html_url' | \
+    curl -s "https://api.github.com/users/$user/starred?page=$page&per_page=$per_page" |   jq -r '.[].html_url' | \
         while read rp; do
                 rp_name=${rp##*/}
                # echo "URL: $rp"
                 if [ ! -d "$rp_name" ]; then
-                        git clone $rp >/dev/null
+                        git clone --quiet $rp
                         cloned=$((cloned+1))
                         echo "$rp_name cloned"
                 else
                         cd $rp_name
-                        git pull >/dev/null
+                        git pull --quiet
                         pulled=$((pulled+1))
                         echo "$rp_name updated"
                 fi
